8486996c600b34f9b9bc55a767cc9ed2c47ec16b
Leo Skye <liangweigain@gmail.com>
2026-02-04 17:09:15 +0800

    added model icons

--- packages/global/common/system/constants.ts
+++ packages/global/common/system/constants.ts
@@ -1,6 +1,6 @@
 export const HUMAN_ICON = `/icon/human.svg`;
 export const LOGO_ICON = `/icon/logo.svg`;
-export const HUGGING_FACE_ICON = `/imgs/model/huggingface.svg`;
+export const HUGGING_FACE_ICON = `model/huggingface`;
 
 export const DEFAULT_TEAM_AVATAR = `/imgs/avatar/defaultTeamAvatar.svg`;
 export const DEFAULT_ORG_AVATAR = '/imgs/avatar/defaultOrgAvatar.svg';
